e188fb8726a3df7d26b79d2421bc6162ff667b02
Derian Melvin <derianmelvin88@gmail.com>
2021-01-02 12:47:14 +0700

    - App.js:   > Set 'completed' with 'id: 2' to 'true'
    - TodoItem.js:
      > Add style into <div> with 'getStyle' function

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       {
         id: 2,
         title: 'Buy groceries',
-        completed: false
+        completed: true
       },
       {
         id: 3,
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      background: '#f0f0f0',
+      padding: '10px',
+      borderBottom: '1px dotted #ccc',
+      textDecoration: this.props.todo.completed ? 'line-through'
+        : 'none'
+    }
+  }
+  
   render() {
     return (
-      <div>
+      <div style={this.getStyle()}>
         <p>{ this.props.todo.title }</p>
       </div>
     )
