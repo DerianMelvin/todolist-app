8661d0e8b100870612e97e85c6f88f33adb26459
Derian Melvin <derianmelvin88@gmail.com>
2021-01-02 13:33:19 +0700

    - App.js:   > Add 'markComplete' function   > Add 'markComplete' property to 'Todos' component
    - Todos.js:
      > Add 'markComplete' property to 'TodoItem' component
    
    - TodoItem.js:
      >  Create & store const value of { id, title } from 'todo'
      > Add <input> into <p> element

--- src/App.js
+++ src/App.js
@@ -24,10 +24,22 @@ class App extends Component {
     ]
   }
 
+  // Toggle todo as either complete or not
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
