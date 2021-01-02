c03441c05833370da456786236afebaf32267011
Derian Melvin <derianmelvin88@gmail.com>
2021-01-02 16:27:05 +0700

    - Create 'About.js' - App.js:   > Import 'About' from 'About.js'   > Import 'Router, Route' from 'react-router-dom'   > Add <Router>   > Move <div> into <Router>   > Add <Route> to <div>   > Add <React.Fragment>   > Move components into <React.Fragment>

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>Todolist v1.0.0</p>
+      <p>This app is part of React crash course</p>
+    </React.Fragment>
+  )
+}
+
+export default About
